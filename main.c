65d98b2f45ba25613a5af255b9f6ea403ed8980d
guiusa <guiusepeoneda@gmail.com>
2024-01-15 12:01:41 -0300

    if no file is provided, read entry from stdin

--- main.c
+++ main.c
@@ -1,39 +1,41 @@
 #include "alloc.h"
 #include "io.h"
+#define ENTRY_SIZE 5000
 
 void showUsage(){
 	printStr("ERROR: Incorrect number of arguments\nUSAGE: >~ bf <source_file_name>\n");
 }
 
 int main (int argc, char** argv) {
+	firstAlloc();
+	char* a = (char *) allocBlk(ENTRY_SIZE);
 	int r = 0;
-	if(argc != 2){
+	fd k;
+
+	if(argc > 2){
 		showUsage();
 		return 1;
-	}
-	firstAlloc();
-	char* a = (char *) allocBlk(80);
-
-	fd k = openFile(argv[1]);
-	if (!k){
-		printStr("Impossible to open file ");
-		printStr(argv[1]);
-		printStr("\n");
-		r = 1;
-		goto free_all;
-	}
+	} else if(argc == 2) {
+		k = openFile(argv[1]);
+		if (!k){
+			printStr("Impossible to open file ");
+			printStr(argv[1]);
+			printStr("\n");
+			r = 1;
+			goto free_all;
+		}
+		if (!readStr(a, ENTRY_SIZE, k)){
+			printStr("Impossible to read stream from file\n");
+			r = 2;
+			goto free_all;
+		}
+	} else readStr(a, ENTRY_SIZE, stdin);
 	
-	int c = readStr(a, 80, k);	
-	if (!c){
-		printStr("Impossible to read stream from file\n");
-		r = 2;
-		goto free_all;
-	}
 	printStr(a);
 
 
 	free_all:
-	closeFile(k);
+	if(k) closeFile(k);
 	freeBlk(a);
 	lastAlloc();
 	return r;
